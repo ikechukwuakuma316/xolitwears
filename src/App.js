cce5cc63f21ffb4fdb5cb861e956f482e25c3893
savvy316 <ikechukwuakuma316@gmail.com>
2021-08-27 19:03:55 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>XOLIT WEARS</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "./Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>XOLIT WEARS</Nav>
+    </>
+  );
+};
+
+export default Navbar;
