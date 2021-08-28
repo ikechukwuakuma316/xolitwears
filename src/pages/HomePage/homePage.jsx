33b53c758866b1231b6e386dfd8a9741d2a63811
copy-ninja1 <emmajnr1000@gmail.com>
2021-08-28 20:25:39 +0100

    fix: add routing to app

--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navbar } from "./components";
+import { Navbar } from "../../components";
 
-function App() {
+function HomePage() {
   return (
     <div className="App">
       <Navbar />
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export { HomePage };
